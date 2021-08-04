f0f7749fdd318479be858abb1e6df1f49465d038
tshihad <shihad-t@hpe.com>
2021-08-04 08:04:54 -0700

    Support for nested structs and arrays

--- tftags.go
+++ tftags.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Get(d *schema.ResourceData, v interface{}) error {
-	rv := reflect.ValueOf(v).Elem()
+	rv := reflect.Indirect(reflect.ValueOf(v))
 	if !rv.CanSet() {
 		return errors.New("only supports pointer structs")
 	}
@@ -19,19 +19,42 @@ func Get(d *schema.ResourceData, v interface{}) error {
 		return errors.New("only struct type is supported")
 	}
 
-	t := rv.Type()
-	for i := 0; i < t.NumField(); i++ {
-		if value, ok := t.Field(i).Tag.Lookup("tf"); ok {
-			splitTags := strings.Split(value, ",")
-			if len(splitTags) < 1 {
-				return errors.New("no proper tag value")
-			}
+	recursive(rv, d, "", nil)
+	return nil
+}
 
-			if result, ok := d.GetOk(splitTags[0]); ok {
-				rv.Field(i).Set(reflect.ValueOf(result))
+func recursive(rv reflect.Value, d *schema.ResourceData, path string, schemaMap interface{}) {
+	switch rv.Kind() {
+	case reflect.Struct:
+		t := rv.Type()
+		for i := 0; i < t.NumField(); i++ {
+			if value, ok := t.Field(i).Tag.Lookup("tf"); ok {
+				splitTags := strings.Split(value, ",")
+				if len(splitTags) < 1 {
+					panic("no proper tag value")
+				}
+				var newPath string
+				if path != "" {
+					newPath = path + "." + splitTags[0]
+				} else {
+					newPath = splitTags[0]
+				}
+				// Get corresponding data from schema
+				if val, ok := d.GetOk(newPath); ok {
+					recursive(rv.Field(i), d, newPath, val)
+				}
 			}
 		}
+	case reflect.Slice:
+		if array, ok := schemaMap.([]interface{}); ok {
+			slice := reflect.MakeSlice(rv.Type(), len(array), cap(array))
+			rv.Set(slice)
+			for i := 0; i < rv.Len(); i++ {
+				recursive(rv.Index(i), d, fmt.Sprintf("%s.%d", path, i), array[i])
+			}
+		}
+	default:
+		fmt.Println(rv.Kind())
+		rv.Set(reflect.ValueOf(schemaMap))
 	}
-
-	return nil
 }
